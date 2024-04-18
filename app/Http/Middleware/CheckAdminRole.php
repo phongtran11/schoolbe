d830563c5853f32d8d90cd482414f3b0167f891e
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2024-04-18 23:05:44 +0700

    fix fix fix

---
+++ app/Http/Middleware/CheckAdminRole.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdminRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        // Kiểm tra xem người dùng có quyền là developer hoặc employer hay không
+        if ($user->account_type === 0) {
+            return $next($request);
+        }
+
+        // Nếu người dùng không có quyền, bạn có thể chuyển hướng hoặc trả về một lỗi tùy ý
+        return response()->json(['error' => 'Unauthorized'], 403);
+    }
+}
